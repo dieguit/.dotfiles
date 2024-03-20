0488a1ef0271d0549debd1507dad0b4d8377143f
Diego Gil <diego.gil2505@gmail.com>
2024-03-20 11:03:19 -0300

    Update lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,53 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    inlay_hints = { enabled = true },
+    --inlay_hints = { enabled = true },
     servers = {
       tsserver = {
+        keys = {
+          {
+            "<leader>ci",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.addMissingImports.ts" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Add missing imports",
+          },
+          {
+            "<leader>cfa",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.fixAll.ts" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Fix all",
+          }
+        },
         settings = {
           typescript = {
             inlayHints = {
-              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHints = "literals",
               includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-              includeInlayFunctionParameterTypeHints = true,
+              includeInlayFunctionParameterTypeHints = false,
               includeInlayVariableTypeHints = false,
               includeInlayVariableTypeHintsWhenTypeMatchesName = false,
               includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = false,
               includeInlayEnumMemberValueHints = true,
             },
           },
           javascript = {
             inlayHints = {
-              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHints = "literals",
               includeInlayParameterNameHintsWhenArgumentMatchesName = false,
               includeInlayFunctionParameterTypeHints = true,
               includeInlayVariableTypeHints = true,
