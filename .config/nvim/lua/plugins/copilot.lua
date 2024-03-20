0045d29b411e6e77ee0eed210cd93c4ca6adf846
Diego Gil <diego.gil2505@gmail.com>
2024-03-20 11:02:01 -0300

    Update copilot.lua

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,28 +1,52 @@
 return {
   {
-    "jellydn/CopilotChat.nvim",
+    "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
-    dependencies = { "zbirenbaum/copilot.lua" }, -- Or { "github/copilot.vim" }
-    opts = {},
-    build = function()
-      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-    end,
-    event = "VeryLazy",
-    keys = {
-      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
-      {
-        "<leader>ccv",
-        ":CopilotChatVisual",
-        mode = "x",
-        desc = "CopilotChat - Open in vertical split",
-      },
-      {
-        "<leader>ccx",
-        ":CopilotChatInPlace<cr>",
-        mode = "x",
-        desc = "CopilotChat - Run in-place code",
-      },
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },        -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },         -- for curl, log wrapper
+      { "nvim-telescope/telescope.nvim" }, -- for telescope help actions (optional)
     },
+    keys = {
+      {
+        "<leader>ccq",
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+          end
+        end,
+        desc = "CopilotChat - Quick chat",
+      },
+      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+      { "<leader>ccT", "<cmd>CopilotChatTests<cr>",   desc = "CopilotChat - Generate tests" },
+      {
+        "<leader>cct",
+        "<cmd>CopilotChatToggle<cr>",
+        desc = "CopilotChat - Toggle", -- Toggle vertical split
+      },
+      {
+        "<leader>ccf",
+        "<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
+        desc = "CopilotChat - Fix diagnostic",
+      },
+      {
+        "<leader>ccr",
+        "<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
+        desc = "CopilotChat - Reset chat history and clear buffer",
+      }
+    },
+    opts = {
+      --debug = true, -- Enable debugging
+      -- See Configuration section for rest
+      -- window = {
+      --   layout = 'float',
+      --   relative = 'cursor',
+      --   width = 0.7,
+      --   height = 0.7,
+      --   row = 1
+      -- }
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }
